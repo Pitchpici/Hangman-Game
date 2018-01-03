efa9e8e281f85c91ebc6b4110051b2a5b6d69ba6
Roxana Milea <roxanamilea@Roxanas-MacBook-Air.local>
2018-01-03 03:02:17 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,10 +19,10 @@
 
     
 
-    o.generateWord = function() {
-      o.incorrectGuesses=[];
-      o.emptyUnderscores=[];
-      o.chances=13;
+    o.generateWord = function() { //this function randomly picks an element from the array, creates two another arrays out of it, and links to html 
+      o.incorrectGuesses=[]; //this is the array where we will save the guessed letters that are not in the randomly generated word
+      o.emptyUnderscores=[]; //the empty array output to the screen
+      o.chances=13; //reiterates the numbers of guesses available for the player, for each game
       document.getElementById("chances").innerHTML=o.chances; 
 
       o.generatedWord= o.array[Math.floor(Math.random()*o.array.length)];
@@ -35,13 +35,12 @@
      
       console.log("this is goin to be in your HTML " + o.emptyUnderscores); 
       document.getElementById("blank").innerHTML=o.emptyUnderscores.join(" ");
-
         
     }  
 
 
-    o.thisIsGuess =function (guess) {
-          var p=0;
+    o.thisIsGuess =function (guess) { //once the player presses a key, we need to check if 1) it is in the generated word
+          var p=0;                    //and 2) if not, it has already been pressed
           var r=0;
           
           for (var j=0; j<o.wordWork.length; j++){ //we check to see if the guessed letter is in the generated word
@@ -54,7 +53,7 @@
               console.log("this is r: " + r);}
 
 
-          if (p>0) { // then I want to change all underscores with the letter
+          if (p>0) { // then I want to change all underscores with the found letter
           
                   for (var i =0; i<o.wordWork.length; i++) {
                     if (o.wordWork[i]==guess) {
@@ -63,13 +62,13 @@
                   } 
           }        
           
-          else if (r==0) {
+          else if (r==0) { //if the letter is not in the already pressed+incorrect keys, I want to include it, plus modify chances
                       o.chances -=1;
                       o.incorrectGuesses.push(guess);
                     }      
-      
+      //update HTML below
 
-        document.getElementById("blank").innerHTML=o.emptyUnderscores.join("");
+        document.getElementById("blank").innerHTML=o.emptyUnderscores.join(""); 
             console.log(o.emptyUnderscores);
         document.getElementById("incorrect").innerHTML=  o.incorrectGuesses.join("*");
             console.log(o.incorrectGuesses);
@@ -77,14 +76,14 @@
 
     } //closing thisIsGuess function
 
-    o.check = function() {
+    o.check = function() { //if player guesses all the letters
 
                 
-              if (o.generatedWord==o.emptyUnderscores.join("")) // if the letters guessed complete the generated word
+              if (o.generatedWord==o.emptyUnderscores.join("")) // if the letters guessed complete the empty array so that it equals generated word
                 {
-                  o.wins++;
-                  document.getElementById("wins").innerHTML=o.wins;
-                  switch (o.generatedWord) {
+                  o.wins++; //increase wins
+                  document.getElementById("wins").innerHTML=o.wins; 
+                  switch (o.generatedWord) { //output a picture of the guessed artist by using switch
                     case "judd": document.getElementById("pics").innerHTML= '<img src="assets/images/judd2.jpg" class="img-fluid">';
                     break;
                     case "mangold": document.getElementById("pics").innerHTML= '<img src="assets/images/mangold.jpg" class="img-fluid">';
@@ -103,11 +102,11 @@
                     break;
                   }
                   alert ("You guessed it!");
-                  o.generateWord();
+                  o.generateWord(); //call function to generate another word
 
                 }
 
-              else if (o.chances===0) {
+              else if (o.chances===0) { //when playes exhausts chances, too bad!
                  document.getElementById("incorrect").innerHTML="";
                  alert("You lost!");
                  o.generateWord();
@@ -115,8 +114,8 @@
               }
     }
 
-      o.generateWord();
-      document.onkeypress= function(event) { //when player presses key, the game begins
+      o.generateWord(); // run function
+      document.onkeypress= function(event) { //when player presses key, we save it, then run through the word array, then see if guessed it
         
           var guess=String.fromCharCode(event.keyCode).toLowerCase();
           document.getElementById("guess").innerHTML="Your guess is " + guess;
